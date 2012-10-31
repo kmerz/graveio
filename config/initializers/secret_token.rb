aac29e682937e9175eba4bd4179b8d97d0b70a3d
Konrad Merz <kmerz@hulud.net>
2012-10-31 16:09:50 +0100

    init the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fox::Application.config.secret_token = 'b2e8b56f43852995554b67257a50a5422d1dc6a825fa3a838e7816429a40dd1d0fb72d5d93980313686724cce4ad43795e6bddf64544aa0a13846d48c667203f'
