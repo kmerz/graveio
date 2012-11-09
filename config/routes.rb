658379d1d12c3e0e79df9ede0b23436f26886b9f
Konrad Merz <kmerz@hulud.net>
2012-11-09 21:27:40 +0100

    generate model for comment and add a route for it
    - create model with:
    $ rails generate model Comment commenter:string body:text post:references
    
    - add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Fox::Application.routes.draw do
   # note that the helper methods are still called with 'posts' instead of 'p'
   # for legibility purposes
-  resources :posts, :path => :p
+  resources :posts, :path => :p do
+    resources :comments
+  end
 
   match '/help' => 'posts#help', :as => :help
   match '/search' => 'posts#search', :as => :search
