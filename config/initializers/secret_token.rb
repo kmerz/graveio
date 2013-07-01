636bdca3ce834c88e45b2af7ebc78512cb947d02
Konrad Merz <kmerz@hulud.net>
2013-07-01 15:57:46 +0200

    Silence deprication warnings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Fox::Application.config.secret_token = 'b2e8b56f43852995554b67257a50a5422d1dc6a825fa3a838e7816429a40dd1d0fb72d5d93980313686724cce4ad43795e6bddf64544aa0a13846d48c667203f'
+Fox::Application.config.secret_key_base = 'b1e8b56f43852995554b67257a50a5422d1dc6a825fa3a838e7816429a40dd1d0fb72d5d93980313686724cce4ad43795e6bddf64544aa0a13846d48c667203f'
