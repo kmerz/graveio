fbd94ce004b2af1c41babe5e6afdbab89006e701
Basti Himmler <sebastian@himmler-tta.de>
2014-02-01 18:13:44 +0100

    Additional validation for model tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,7 @@
 class Tag < ActiveRecord::Base
 
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :case_sensitive => false
+  validates_format_of :name, :with => /\A[-a-zA-Z0-9]+\Z/
 
   has_many :post_tags
   has_many :posts, :through => :post_tags
