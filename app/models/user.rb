e260556995b28a810aa272c65c0e01b682d04f3f
Maximilian Fischer <mafi@inagile.org>
2014-02-01 21:50:11 +0100

    Add api_key generation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def generate_apikey
+    self.api_key = Base64.encode64(OpenSSL::Random.random_bytes(33)).chomp
+    self.save
+  end
+
 end
