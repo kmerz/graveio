9b17d627a05ea16ea8ca3522de2817ff9d8726e2
Maximilian Fischer <mafi@inagile.org>
2014-02-01 23:16:59 +0100

    Add apikey generation on registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def generate_apikey
+  def generate_api_key
     self.api_key = Base64.encode64(OpenSSL::Random.random_bytes(33)).chomp
     self.save
   end
