7abb4c4e7b25afab8a8ff4dc49f21e74d969f988
Maximilian Fischer <mafi@inagile.org>
2014-02-02 02:26:04 +0100

    Add apikey integration and regenerate of apikey

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :apikey
+
+  def add_new_api_key
+    if self.apikey.present?
+      self.apikey.destroy
+    end
+    api_key = Apikey.new
+    api_key.user_id = self.id
+    api_key.save
+
+    self.apikey_id = api_key.id
+    self.save
+  end
 end
